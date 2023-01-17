c76d85ffbf4bac51d3f9259ea425264e4756db09
Aditya Jyoti <110086332+Adityajyoti002@users.noreply.github.com>
2023-01-17 23:36:46 +0100

    push at bottom functiom concepts used recursion reverse a string

--- Stackclass3.java
+++ Stackclass3.java
@@ -11,13 +11,22 @@ public class Stackclass3 {
         s.push(top);
 
     }
+
+    public static void reverse(Stack<Integer> s){
+        if (s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+    }
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
 
-        pushAtBottom(4, s);
+        reverse(s);
 
         while (!s.isEmpty()){
             System.out.println(s.peek());
