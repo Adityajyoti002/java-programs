406ce940cee76fad72efc544715beaeca23bec6a
Aditya Jyoti <110086332+Adityajyoti002@users.noreply.github.com>
2023-01-20 23:01:09 +0100

    classes and ocjecrs

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+        String name;
+        int age;
+
+        public void getInfo() {
+            System.out.println("The name of this Student is " + this.name);
+            System.out.println("The age of this Student is " + this.age);
+        }
+    }
+
+
